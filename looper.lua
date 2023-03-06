2fc8bf4c5696d16c9fb2e327a84920152a3d2a56
dondelion666 <91913477+dondelion666@users.noreply.github.com>
2023-03-06 21:37:13 +0000

    Update looper.lua

--- looper.lua
+++ looper.lua
@@ -1,27 +1,36 @@
 function init()
 
-  softcut.enable(1,1)
-  softcut.level_input_cut(1,1,1)
-  softcut.play(1,1)
-  softcut.rate(1,1)
-  softcut.rec(1,1)
-  softcut.buffer(1,1)
-  softcut.position(1,1)
-  
+for i=1,4 do
+  softcut.enable(i,1)
+  softcut.level_input_cut(1,i,1)
+  softcut.play(i,1)
+  softcut.rec(i,1)
+  softcut.buffer(i,1)
+  softcut.position(i,1)
 end
 
-
-params:add_control("start","start",controlspec.new(1,10,'lin',0.01,0,'s',0.01))
-params:set_action("start",function(x) softcut.loop_start(1,x) end)
-params:add_control("end","end",controlspec.new(10,10,'lin',0.01,10,'s',0.01))
-params:set_action("end",function(x) softcut.loop_end(1,x) end)
-params:add_number("looping","looping",0,1,1)
-params:set_action("looping",function (x) softcut.loop(1,x) end)
-params:add_control("rec_level","rec_level",controlspec.new(0,1,'lin',0.01,1,'s',0.01))
-params:set_action("rec_level",function(x) softcut.rec_level(1,x) end)
-params:add_control("pre_level","rec_level",controlspec.new(0,1,'lin',0.01,0.5,'s',0.01))
-params:set_action("pre_level",function(x) softcut.pre_level(1,x) end)
-params:add_control("out_level","rec_level",controlspec.new(0,1,'lin',0.01,1,'s',0.01))
-params:set_action("out_level",function(x) softcut.level(1,x) end)
-
+for i=1,4 do
+  params:add_separator("voice"..i,"voice"..i)
+  params:add_control("start"..i,"start"..i,controlspec.new(1,10,'lin',0.01,0,'s',0.01))
+  params:set_action("start"..i,function(x) softcut.loop_start(i,x) end)
+  params:add_control("end"..i,"end"..i,controlspec.new(1,10,'lin',0.01,10,'s',0.01))
+  params:set_action("end"..i,function(x) softcut.loop_end(i,x) end)
+  params:add_number("rate"..i,"rate"..i,-64,64,0)
+  params:set_action("rate"..i, function(x) softcut.rate(i,interval_to_ratio(x)) end)
+  params:add_number("looping"..i,"looping"..i,0,1,1)
+  params:set_action("looping"..i,function (x) softcut.loop(i,x) end)
+  params:add_control("rec_level"..i,"rec_level"..i,controlspec.new(0,1,'lin',0.01,0,'s',0.01))
+  params:set_action("rec_level"..i,function(x) softcut.rec_level(i,x) end)
+  params:add_control("pre_level"..i,"pre_level"..i,controlspec.new(0,1,'lin',0.01,0.5,'s',0.01))
+  params:set_action("pre_level"..i,function(x) softcut.pre_level(i,x) end)
+  params:add_control("out_level"..i,"out_level"..i,controlspec.new(0,1,'lin',0.01,1,'s',0.01))
+  params:set_action("out_level"..i,function(x) softcut.level(i,x) end)
+end
+  
 params:bang()
+
+end
+
+function interval_to_ratio(interval)
+  return math.pow(2, interval / 12)
+end
